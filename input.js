3a1ea816e1c83ed4184e3278ed1452241e99ae1d
Etienne LC <etlecoent@gmail.com>
2020-09-23 18:15:22 +0000

    Directions are now written to the connection

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,6 +13,14 @@ const setupInput = function() {
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === '\u0077') {
+    connection.write('Move: up');
+  } else if (key === '\u0073') {
+    connection.write('Move: down');
+  } else if (key === '\u0061') {
+    connection.write('Move: left');
+  } else if (key === '\u0064') {
+    connection.write('Move: right');
   }
 };
 
